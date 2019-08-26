0dcb8ce3aa728912551e5b6fb1017dd0f15947b9
tanmaysingh18 <53615354+tanmaysingh18@users.noreply.github.com>
2019-08-26 22:52:12 +0530

    Create A1q7.java

---
+++ A1q7.java
@@ -0,0 +1,33 @@
+public class A1q7{
+ 
+   public static void main(String[] args)
+   {
+   char n='+';
+   float a,b;
+   a=6;b;
+   float c=0;
+   switch(n)
+   {
+   case'+':
+   c=a+b;
+   break;
+   case'-':
+   c=a-b;
+   break;
+   case'*':
+   c=a*b;
+   break;
+   case'\':
+   c=a\b;
+   break;
+   default:
+   System.out.println("invalid");
+   }
+   System.out.println("The calculation is="+c);   
+   }
+   
+   
+   
+   
+   
+   
