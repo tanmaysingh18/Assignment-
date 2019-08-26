41bc9383dccfa420814459b0974d6fad6bb39aa1
tanmaysingh18 <53615354+tanmaysingh18@users.noreply.github.com>
2019-08-26 22:31:56 +0530

    Update A1q6a.java

--- A1q6a.java
+++ A1q6a.java
@@ -3,13 +3,13 @@ public class A1q6a {
 
 	public static void main(String[] args) {
 int year=2300;
-if(year%4==0&&year%100==0&&year%400==0)
+if(year%4==0||year%100==0&&year%400==0)
 {
 	
 		
 				System.out.println("a leap year");	 }
 else {
-			System.out.println(" not Leap y");
+			System.out.println(" not Leap year");
 		
 }
     
